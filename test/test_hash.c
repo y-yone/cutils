880a1a1bc443968e03832ad44976aea4bece46e9
y-yone <mfmf.soft@gmail.com>
2019-10-28 00:14:17 +0900

    Add user_data to hash.

--- test/test_hash.c
+++ test/test_hash.c
@@ -11,13 +11,13 @@ int main(int argc, char *argv[])
     char test3[] = "test1";
     char *test;
 
-    hash = hash_new(256);
+    hash = hash_new(256, 0);
     if (hash == NULL) {
         printf("hash_new fail\n");
     }
 
-    ret = hash_add(hash, test1, sizeof(test1));
-    ret = hash_add(hash, test2, sizeof(test2));
+    ret = hash_add(hash, test1, sizeof(test1), NULL);
+    ret = hash_add(hash, test2, sizeof(test2), NULL);
 
     test = hash_search(hash, test3, sizeof(test3));
     if (test != test1) {
