9526673340cbc21a570333cfc5ede6ae5d094bf6
y-yone <mfmf.soft@gmail.com>
2019-11-03 11:52:21 +0900

    add btree test.

---
+++ test/test_btree.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include "tree/btree.h"
+
+int compare(void *ctx, void *data)
+{
+    int a, b;
+
+    a = *(int *)ctx;
+    b = *(int *)data;
+
+    return b - a;
+}
+
+void tree_free(void *udata)
+{
+    return;
+}
+
+
+int tree_build(void *uctx, void *udata)
+{
+    int *usr, *ctx;
+
+    usr = udata;
+    ctx = uctx;
+
+    *usr = *ctx;
+    return 0;
+}
+
+
+int main(int argc, char *argv[])
+{
+    int ret;
+    btree_root_t *root;
+    btree_elem_t *elem;
+    
+    int a = 1;
+    int b = 10;
+    int c = 5;
+
+    root = btree_new(sizeof(int), compare);
+    if (root == NULL) {
+        printf("btree_new fail\n");
+    }
+
+    root->tree_user = tree_build;
+    root->tree_free = tree_free;
+
+    elem = btree_add(root, &a);
+    elem = btree_add(root, &b);
+
+    elem = btree_search(root, &a);
+    if (elem != NULL) {
+        printf("elem data %d\n", *(int*)elem->user_data);
+    }
+
+    return 0;
+}
